74bdc8428c915f13757cad81be8dde7e8864997f
Mahendra Kolhe <mkolhe23@gmail.com>
2025-08-12 20:42:33 +0530

    update: modified config/config.py

--- config/config.py
+++ config/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 CREDENTIALS_PATH = f"{BASE_DIR}/secrets/{os.getenv('CREDENTIALS_FILENAME')}"
-SCOPES = SCOPES = os.getenv("SCOPES").split(",")
+SCOPES = os.getenv("SCOPES").split(",")
 
 SERVER = os.getenv('SERVER')
 USER = os.getenv('USER')
